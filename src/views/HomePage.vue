706e7b9699bbfd8457c6028ba85b136d94009f01
Michel Lobato <michelg.lobato@gmail.com>
2023-06-02 16:12:23 -0300

    Ultima mudanca~

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -27,5 +27,37 @@ export default {
 </script>
 
 <style>
-/* Estilos para a página inicial */
+  .title {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+
+  .button-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+  }
+
+  .button-container button {
+    margin-bottom: 10px;
+    padding: 10px 20px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+    background-color: #f0c14b;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .button-container button:hover {
+    background-color: #e3b243;
+  }
+
+  .button-container button:last-child {
+    margin-bottom: 0;
+  }
 </style>
+
